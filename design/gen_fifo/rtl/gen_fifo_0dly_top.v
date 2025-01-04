dc86465cb731b4221569697de938d7a5c27102ce
dodlido <etaysela@gmail.com>
2025-01-04 19:01:27 +0200

    added view to gen_fifo: 0-delay FIFO

---
+++ design/gen_fifo/rtl/gen_fifo_0dly_top.v
@@ -0,0 +1,127 @@
+
+module gen_fifo_0dly_top #(
+   // Parameters // 
+   // ---------- //
+   parameter  DEPTH = 16            , // FIFO depth 
+   parameter  DAT_W = 4             , // Data width [bits]
+   localparam ADD_W = $clog2(DEPTH) , // Pointers width [bits]
+   localparam CNT_W = ADD_W + 1       // Counter width [bits]
+)(
+   // General // 
+   // ------- // 
+   input  logic [      0:0] clk        , // Clock signal 
+   input  logic [      0:0] rst_n      , // Async reset, active low
+
+   // Configurations //
+   // -------------- // 
+   input  logic [CNT_W-1:0] cfg_af_th  , // almost-full threshold, anything including and above this value will assert sts_af
+   input  logic [CNT_W-1:0] cfg_ae_th  , // almost-empty threshold, anything including and below this value will assert sts_ae
+
+   // Input Controls // 
+   // -------------- //
+   input  logic [      0:0] push       , // Write enable active high
+   input  logic [      0:0] pop        , // Output enable active high
+
+   // Data Path //
+   // --------- //
+   input  logic [DAT_W-1:0] dat_in     , // Input data
+   output logic [DAT_W-1:0] dat_out    , // Output data
+
+   // Output Statuses //
+   // --------------- //
+   output logic [CNT_W-1:0] sts_count  , // FIFO count
+   output logic [      0:0] sts_full   , // FIFO full
+   output logic [      0:0] sts_af     , // FIFO almost-full
+   output logic [      0:0] sts_ae     , // FIFO almost-empty
+   output logic [      0:0] sts_empty  , // FIFO empty
+   output logic [      0:0] err_ovfl   , // error - overflow detected
+   output logic [      0:0] err_udfl     // error - underflow detected
+);
+
+// Internal Wires //
+// -------------- //
+logic [DAT_W-1:0] int_fifo_dat_in   ; 
+logic [DAT_W-1:0] int_fifo_dat_out  ; 
+logic             int_fifo_push     ;
+logic             int_fifo_pop      ;
+logic [CNT_W-1:0] int_fifo_count    ; 
+logic             int_fifo_full     ;
+logic             int_fifo_empty    ;
+logic [DAT_W-1:0] skid_buff_dat_in  ;
+logic [DAT_W-1:0] skid_buff_dat_out ;
+logic             skid_buff_vld_in  ; 
+logic             skid_buff_rdy_in  ; 
+logic             skid_buff_rdy_out ; 
+logic             skid_buff_vld_out ; 
+
+// Internal FIFO, depth of DEPTH-1 //
+// ------------------------------- //
+// Input control logic //
+assign int_fifo_push   = push & ~byps_fifo_cond ; 
+assign int_fifo_pop    = skid_buff_rdy_out & ~int_fifo_empty ; 
+assign int_fifo_dat_in = dat_in ; 
+// FIFO instance //
+gen_fifo_top #(.DEPTH(DEPTH-1), .DAT_W(DAT_W)) i_gen_fifo_top (
+      // Parameters // 
+      // General // 
+   .clk       (clk               ), // i, 0:0   X logic  , Clock signal
+   .rst_n     (rst_n             ), // i, 0:0   X logic  , Async reset. active low
+      // Configurations //
+   .cfg_af_th ({CNT_W{1'b1}}     ), // i, CNT_W X logic  , almost-full threshold. anything including and above this value will assert sts_af
+   .cfg_ae_th ({CNT_W{1'b0}}     ), // i, CNT_W X logic  , almost-empty threshold. anything including and below this value will assert sts_ae
+      // Input Controls // 
+   .clr       (1'b0              ), // i, 0:0   X logic  , Clear FIFO. reset all pointers to 0
+   .push      (int_fifo_push     ), // i, 0:0   X logic  , Write enable active high
+   .pop       (int_fifo_pop      ), // i, 0:0   X logic  , Output enable active high
+      // Data Path //
+   .dat_in    (int_fifo_dat_in   ), // i, DAT_W X logic  , Input data
+   .dat_out   (int_fifo_dat_out  ), // o, DAT_W X logic  , Output data
+      // Output Statuses //
+   .sts_count (int_fifo_count    ), // o, CNT_W X logic  , FIFO count
+   .sts_full  (int_fifo_full     ), // o, 0:0   X logic  , FIFO full
+   .sts_af    (                  ), // o, 0:0   X logic  , FIFO almost-full
+   .sts_ae    (                  ), // o, 0:0   X logic  , FIFO almost-empty
+   .sts_empty (int_fifo_empty    ), // o, 0:0   X logic  , FIFO empty
+   .err_ovfl  (                  ), // o, 0:0   X logic  , error - overflow detected
+   .err_udfl  (                  )  // o, 0:0   X logic  , error - underflow detected
+);
+
+// 1-level Skid Buffer at the output level //
+// --------------------------------------- // 
+// Buffer input logic // 
+assign byps_fifo_cond   = skid_buff_rdy_out &  int_fifo_empty & push           ; // If the FIFO is empty and the skid buffer is empty we can bypass the internal FIFO 
+assign skid_buff_dat_in = byps_fifo_cond ? dat_in : int_fifo_dat_out           ; // In case of bypassing the FIFO, skid buffer input is the main input data 
+assign skid_buff_vld_in = byps_fifo_cond | skid_buff_rdy_out & ~int_fifo_empty ; // valid in case bypass of internal FIFO or the internal FIFO is not empty and buffer is
+assign skid_buff_rdy_in = pop                                                  ; // simply use the pop
+// Buffer instance // 
+gen_skid_buff_top #(.DAT_W(DAT_W)) i_gen_skid_buff_top (
+       // General // 
+   .clk     (clk              ), // i, 0:0   X wire  , clock signal
+   .rst_n   (rst_n            ), // i, 0:0   X wire  , Async reset. active low
+       // Input >--> Buffer // 
+   .dat_in  (skid_buff_dat_in ), // i, DAT_W X wire  , Input data
+   .vld_in  (skid_buff_vld_in ), // i, 0:0   X wire  , Input valid indicator
+       // Output >--> Buffer //, 
+   .rdy_in  (skid_buff_rdy_in ), // i, 0:0   X wire  , Output ready indicator
+       // Buffer >--> Input // , 
+   .rdy_out (skid_buff_rdy_out), // o, 0:0   X reg   , Input ready indicator
+       // Buffer >--> Output // 
+   .vld_out (skid_buff_vld_out), // o, 0:0   X reg   , Output valid indicator
+   .dat_out (skid_buff_dat_out)  // o, DAT_W X reg   , Output data
+);
+
+// Output Level // 
+// ------------ //
+assign dat_out   = skid_buff_dat_out ; 
+// Count // 
+assign sts_count = int_fifo_count + {{(CNT_W-1){1'b0}}, skid_buff_rdy_out} ; 
+// Statuses // 
+assign sts_empty = ~skid_buff_vld_out ; 
+assign sts_ae    = sts_count <= cfg_ae_th ; 
+assign sts_af    = sts_count >= cfg_af_th ; 
+assign sts_full  = int_fifo_full & ~skid_buff_rdy_out ; 
+// Errors // 
+assign err_ovfl  = push & sts_full  ; 
+assign err_udfl  = pop  & sts_empty ;
+
+endmodule
--- design/gen_fifo/rtl/gen_skid_buff_top.v
+++ design/gen_fifo/rtl/gen_skid_buff_top.v
@@ -1,6 +1,6 @@
 //|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|//
 //|                                                                                    |//
-//| ~~ skid_buff ~~                                                                    |//
+//| ~~ gen_skid_buff_top ~~                                                            |//
 //|                                                                                    |//
 //| Top-level description:                                                             |//
 //|    1. Data-vld-rdy skid buffer, rdy signal registered                              |//
@@ -19,34 +19,34 @@
 //|                                                                                    |//
 //|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|//
 
-module skid_buff #(
-    parameter int       DAT_W       =    8 , // Data width in bits
+module gen_skid_buff_top #(
+    parameter int       DAT_W       =    4 , // Data width in bits
     parameter bit [0:0] OPT_OUT_REG = 1'b0   // Optional output registers
 ) (
     // General // 
-    input wire [      0:0] clk     , // clock signal
-    input wire [      0:0] rst_n   , // Async reset, active low
+    input  logic [      0:0] clk     , // clock signal
+    input  logic [      0:0] rst_n   , // Async reset, active low
     // Input >--> Buffer // 
-    input wire [DAT_W-1:0] dat_in  , // Input data
-    input wire [      0:0] vld_in  , // Input valid indicator
+    input  logic [DAT_W-1:0] dat_in  , // Input data
+    input  logic [      0:0] vld_in  , // Input valid indicator
     // Output >--> Buffer // 
-    input wire [      0:0] rdy_in  , // Output ready indicator
+    input  logic [      0:0] rdy_in  , // Output ready indicator
     // Buffer >--> Input // 
-    output reg [      0:0] rdy_out , // Input ready indicator
+    output logic [      0:0] rdy_out , // Input ready indicator
     // Buffer >--> Output // 
-    output reg [      0:0] vld_out , // Output valid indicator
-    output reg [DAT_W-1:0] dat_out   // Output data
+    output logic [      0:0] vld_out , // Output valid indicator
+    output logic [DAT_W-1:0] dat_out   // Output data
 );
 
 // Internal regs // 
-reg  [      0:0] full ; // Buffer is full
-reg  [DAT_W-1:0] buff ; // Internal buffer
+logic [      0:0] full ; // Buffer is full
+logic [DAT_W-1:0] buff ; // Internal buffer
 
 // Internal wires // 
-wire [      0:0] full_next    ; // Next full value
-wire [DAT_W-1:0] buff_next    ; // Next buff value
-wire [      0:0] vld_out_next ; // Next vld_out
-wire [DAT_W-1:0] dat_out_next ; // Next dat_out value
+logic [      0:0] full_next    ; // Next full value
+logic [DAT_W-1:0] buff_next    ; // Next buff value
+logic [      0:0] vld_out_next ; // Next vld_out
+logic [DAT_W-1:0] dat_out_next ; // Next dat_out value
 
 // Internal logic //
 assign full_next    = (vld_in & ~rdy_in) ? 1'b1   : (full & rdy_in) ? 1'b0 : full          ; // not ready for new input ==> full, ready and full ==> empty
@@ -56,57 +56,21 @@ assign dat_out_next = (vld_in &  rdy_in) ? dat_in : (full & rdy_in) ? buff : {DA
 
 // Generate output registers or assigns // 
 generate
-    if (OPT_OUT_REG) begin : gen_smpl_out_case
-        base_reg #(.WID(DAT_W)) i0_base_reg (
-            .clk      (clk         ),
-            .rst_n    (rst_n       ),
-            .en       (1'b1        ),
-            .data_in  (dat_out_next),
-            .data_out (dat_out     )
-        ); // data FFs 
-        base_reg #(.WID(1)) i1_base_reg (
-            .clk      (clk         ),
-            .rst_n    (rst_n       ),
-            .en       (1'b1        ),
-            .data_in  (vld_out_next),
-            .data_out (vld_out     )
-        ); // valid FF
-    end
-    else begin : gen_no_smpl_out_case
-        assign dat_out = dat_out_next ; // dat connection
-        assign vld_out = vld_out_next ; // vld connection
-    end
+   if (OPT_OUT_REG) begin : gen_smpl_out_case
+      always_ff @(posedge clk) if (!rst_n) dat_out <= DAT_W'(0) ; else dat_out <= dat_out_next ; 
+      always_ff @(posedge clk) if (!rst_n) vld_out <= 1'b0      ; else vld_out <= vld_out_next ; 
+   end
+   else begin : gen_no_smpl_out_case
+      assign dat_out = dat_out_next ; 
+      assign vld_out = vld_out_next ; 
+   end
 endgenerate
 
-// Full FF // 
-base_reg #(.WID(1)) i2_base_reg (
-    .clk      (clk         ),
-    .rst_n    (rst_n       ),
-    .en       (1'b1        ),
-    .data_in  (rdy_in      ),
-    .data_out (rdy_out     )
-); 
-
-// Buffer FFs // 
-base_reg #(.WID(DAT_W)) i3_base_reg (
-    .clk      (clk         ),
-    .rst_n    (rst_n       ),
-    .en       (1'b1        ),
-    .data_in  (buff_next   ),
-    .data_out (buff        )
-); 
-
-// Full FF // 
-base_reg #(.WID(1)) i4_base_reg (
-    .clk      (clk         ),
-    .rst_n    (rst_n       ),
-    .en       (1'b1        ),
-    .data_in  (full_next   ),
-    .data_out (full        )
-); 
-
-
-
+// FFs //
+// --- //
+always_ff @(posedge clk) if (!rst_n) rdy_out <= 1'b0      ; else rdy_out   <= rdy_in    ; 
+always_ff @(posedge clk) if (!rst_n) buff    <= DAT_W'(0) ; else buff      <= buff_next ; 
+always_ff @(posedge clk) if (!rst_n) full    <= 1'b0      ; else full      <= full_next ; 
 
 endmodule
 
